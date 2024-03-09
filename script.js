63d560704dfbb7bb3d9ebbe2fb78c284013d4531
Vladi <39379713+vladik93@users.noreply.github.com>
2024-03-09 16:31:41 +0200

    adding animations to hero

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('script.js is running');
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var scrollpos = localStorage.getItem('scrollpos');
+  if (scrollpos) window.scrollTo(0, scrollpos);
+});
+
+window.onbeforeunload = function(e) {
+  localStorage.setItem('scrollpos', window.scrollY);
+};
