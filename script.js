c336580f89438dad76a2361d0e46d38933fd6ae7
Vladi <39379713+vladik93@users.noreply.github.com>
2024-03-10 21:17:09 +0200

    sidenav animations

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 console.log('script.js is running');
 
-window.onbeforeunload = function() {
-  window.scrollTo(0, 0);
-};
+// SIDENAV
+const togglerEl = document.getElementById("toggler");
+
+// SIDENAV
+const sidenavOverlayEl = document.getElementById('sidenav-overlay');
+const sidenavEl = document.getElementById('sidenav');
+
+
+togglerEl.addEventListener('click', (e) => {
+  togglerEl.classList.toggle('open');
+  sidenavOverlayEl.classList.toggle('open');
+  sidenavEl.classList.toggle('open');
+  document.body.style.overflow = "hidden";
+})
+
