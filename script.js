38b6ed5def45aedbd80d69df83b9ddd19bdbac5d
Vladi <39379713+vladik93@users.noreply.github.com>
2024-03-25 11:21:29 +0200

    pricing-slider-dynamic-color
    status: complete

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ const togglerEl = document.getElementById("toggler");
 const sidenavOverlayEl = document.getElementById('sidenav-overlay');
 const sidenavEl = document.getElementById('sidenav');
 
-
-
-
 togglerEl.addEventListener('click', (e) => {
   togglerEl.classList.toggle('open');
   sidenavOverlayEl.classList.toggle('open');
@@ -35,6 +32,7 @@ skillTransitionEls.forEach(skillTransitEl => {
 })
 
 // PRICING SECTION
+const pricingSectionEl = document.getElementById('pricing');
 const pricingWrapperEl = document.getElementById('pricing-wrapper');
 const pricingInnerWrapperEl = document.getElementById('pricing-inner-wrapper');
 
@@ -45,19 +43,47 @@ let pricingItemCurrent = 0;
 const pricingSlideBtnBack = document.getElementById('pricing-slide-button-back');
 const pricingSlideBtnForward = document.getElementById('pricing-slide-button-forward');
 
+
+const setPricingColor = () => {
+  console.log(pricingItemCurrent);
+  // 
+  switch(pricingItemCurrent) {
+    case 0 : {
+      pricingSectionEl.classList = "pricing basic";
+      break;
+    }
+
+    case 1 : {
+      pricingSectionEl.classList = "pricing pro";
+      break;
+    }
+
+    case 2 : {
+      pricingSectionEl.classList = "pricing premium";
+      break;
+    }
+  }
+}
+
 pricingSlideBtnForward.addEventListener('click', () => {
   if(pricingItemCurrent < pricingItemEls.length - 1) {
     pricingItemCurrent++;
 
     pricingInnerWrapperEl.style.transform = `translateX(-${pricingItemCurrent * 100}%)`;
+
   }
+
+  setPricingColor();
+
 });
 
 pricingSlideBtnBack.addEventListener('click', () => {
   if(pricingItemCurrent > 0) {
     pricingItemCurrent--;
 
-    pricingInnerWrapperEl.style.transform = `translateX(-${pricingItemCurrent * 100}%)`;
+    pricingInnerWrapperEl.style.transform = `translateX(-${pricingItemCurrent * 100}%)`;    
   }
+
+  setPricingColor();
 })
 
